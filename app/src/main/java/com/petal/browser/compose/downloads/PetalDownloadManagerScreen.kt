249e702975cfd822a95a410ccc5b255e88c90737
Shrey Agarwal <sanjayvansu1973@gmail.com>
2026-08-12 07:58:51 +0530

    Add import androidx.compose.ui.unit.dp to PetalDownloadManagerScreen.kt

--- app/src/main/java/com/petal/browser/compose/downloads/PetalDownloadManagerScreen.kt
+++ app/src/main/java/com/petal/browser/compose/downloads/PetalDownloadManagerScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.setViewTreeLifecycleOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
